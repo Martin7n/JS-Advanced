4d96837fe4fedb5303fdac1b56af7f1309d5dbff
Martin7n <122726440+Martin7n@users.noreply.github.com>
2025-04-03 12:51:52 +0300

    tasks...

--- JS applications/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/04.Locked-Profile/app.js
+++ JS applications/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/04.Locked-Profile/app.js
@@ -1,3 +1,64 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+
+    const mainUrlProfilesInfo = "http://localhost:3030/jsonstore/advanced/profiles/"
+    const divProfile = document.getElementsByClassName('profile')[0];
+    
+    const users = await getProfiles(mainUrlProfilesInfo);
+
+    Object.values(users).forEach(element => {
+        profileConstructor(element)
+        console.log(`username: ${element.username}, email: ${element.email}, age: ${element.age}`)
+        
+    });
+
+    divProfile.remove()
+
+
+
+    async function getProfiles(mainUrlProfilesInfo) {
+        const response = await fetch(mainUrlProfilesInfo)
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} (${response.statusText})`)
+        }
+        return await response.json()
+    }
+
+
+    
+    function profileConstructor(userData){
+        const clone = divProfile.cloneNode(true);
+        clone.querySelector('input[name="user1Username"]').value =  userData.username;
+        clone.querySelector('input[name="user1Email"]').value =  userData.email;
+        clone.querySelector('input[name="user1Age"]').value =  userData.age;
+        clone.querySelector('input[name="user1Age"]').textContent =  userData.age;
+
+        clone.getElementsByClassName("user1Username")[0].style.display = "none"
+        
+        const showBtn =  clone.getElementsByTagName('button')[0] 
+
+        showBtn.addEventListener("click", (event) => {
+
+            let checked = showBtn.parentElement.querySelector('input[type="radio"]:checked')
+            let div = showBtn.parentElement.getElementsByClassName("user1Username")[0]
+
+
+            if (checked.value === "lock"){
+                console.log("LOCK")
+            } else if(showBtn.textContent === "Show more"){
+                div.style.display = "inline-block"
+                showBtn.textContent = "Hide it"
+                console.log(div)
+                
+            } else {
+                div.style.display = "none"
+                showBtn.textContent = "Show more"
+            } 
+        
+        })
+            
+       
+        main.appendChild(clone);
+
+    }
+
 }
--- JS applications/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
+++ JS applications/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
@@ -1,5 +1,33 @@
 function attachEvents() {
-    console.log('TODO...');
+
+
+    const basePosts = "http://localhost:3030/jsonstore/blog/posts";
+    const baseComments = "http://localhost:3030/jsonstore/blog/comments";
+
+
+    const loadBtn = document.getElementById("btnLoadPosts")
+    const viewPostBtn = document.getElementById("btnViewPost");
+
+
+    fetch(basePosts)
+    .then(response => {
+        if (!response.ok){throw new Error()
+        } else {return response.json()}})
+    .then(postData)
+    .catch();
+
+    function postData(data){
+
+      for (const key in data) {
+        const element = data[key];
+        console.log(element.body)
+      }
+        
+   
+        
+    
+    }
+    
 }
 
 attachEvents();
