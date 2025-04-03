99bc4f5a838c6e9b46422f0d50c0b5df47074d7c
Martin7n <122726440+Martin7n@users.noreply.github.com>
2025-04-03 17:01:55 +0300

    tasks

--- JS applications/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
+++ JS applications/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
@@ -1,13 +1,25 @@
 function attachEvents() {
 
 
-    const basePosts = "http://localhost:3030/jsonstore/blog/posts";
-    const baseComments = "http://localhost:3030/jsonstore/blog/comments";
+    const basePosts = "http://localhost:3030/jsonstore/blog/posts/";
+    const baseComments = "http://localhost:3030/jsonstore/blog/comments/";
+
 
 
     const loadBtn = document.getElementById("btnLoadPosts")
     const viewPostBtn = document.getElementById("btnViewPost");
+    const selectPosts = document.getElementById('posts');
 
+    const postTitle = document.getElementById("post-title")
+    const postContent = document.getElementById("post-body")
+    const ulElmnt = document.getElementById('post-comments');
+    let selectedPostID;
+    let urll;
+
+    loadBtn.addEventListener("click", loadAllPosts)
+
+    function loadAllPosts(){
+        clearPrev()
 
     fetch(basePosts)
     .then(response => {
@@ -16,18 +28,79 @@ function attachEvents() {
     .then(postData)
     .catch();
 
+    }
+
+
     function postData(data){
 
       for (const key in data) {
         const element = data[key];
         console.log(element.body)
-      }
-        
-   
-        
-    
-    }
-    
-}
 
+        const newOption = document.createElement('option');
+        newOption.value = element.id;
+        newOption.textContent = element.title;
+
+        selectPosts.appendChild(newOption)
+      };
+        
+    
+
+
+      viewPostBtn.addEventListener("click", loadPost);
+
+      function loadPost(event){
+       
+        selectedPostID = selectPosts.value;
+        console.log(selectedPostID)
+        let urlPost = basePosts+selectedPostID
+     
+        fetch(urlPost)
+        .then(response => {if (!response.ok){throw new Error()} else {return response.json()}})
+        .then(postDetails)
+        .catch();
+
+        fetch(baseComments)
+        .then(response => {if (!response.ok){throw new Error()} else {return response.json()}})
+        .then(commentDetails)
+        .catch();
+
+      }
+
+    }
+
+    function postDetails(pstData){
+        postTitle.textContent = pstData.title
+        postContent.textContent = pstData.body 
+        console.log(pstData)
+    };
+
+    function clearPrev(){
+        postTitle.textContent = "Post Details"
+        postContent.textContent = ""
+        ulElmnt.replaceChildren()
+        
+        selectPosts.replaceChildren()
+
+    }
+      
+    function commentDetails(comData){
+
+        
+        
+        for (const key in comData) {
+
+            const element = comData[key];
+           if (element.postId === selectedPostID){
+            console.log(element.text)
+
+            const li = document.createElement('li')
+            li.textContent = element.text;
+            ulElmnt.append(li)
+                
+            }
+        }
+    };
+
+}
 attachEvents();
